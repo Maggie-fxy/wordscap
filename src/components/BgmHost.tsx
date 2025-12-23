de37ea4410467307078f35f409ba1a18db760507
Maggie-fxy <897858381@qq.com>
2025-12-24 00:55:21 +1400

    fix: add iframe permissions for BGM player

--- src/components/BgmHost.tsx
+++ src/components/BgmHost.tsx
@@ -24,7 +24,8 @@ function BgmIframe() {
         marginHeight={0}
         width={1}
         height={1}
-        allow="autoplay"
+        allow="autoplay; encrypted-media; accelerometer; gyroscope"
+        referrerPolicy="no-referrer-when-downgrade"
         src="https://music.163.com/outchain/player?type=2&id=2075140388&auto=1&height=66"
       />
     </div>
